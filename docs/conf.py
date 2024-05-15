accb203723b298f2f5e4caf562770d0191116f13
SVJ_Vitor <shenvitor@hotmail.com>
2024-05-15 14:58:23 +0200

    DX: add workflows including MyST NB and precommit (#9)

---
+++ docs/conf.py
@@ -0,0 +1,11 @@
+exclude_patterns = [
+    "**.ipynb_checkpoints",
+    ".DS_Store",
+    ".pixi",
+    "Thumbs.db",
+    "_build",
+]
+extensions = ["myst_nb"]
+master_doc = "index"
+nitpicky = True
+project = "GlueX N-Star"
