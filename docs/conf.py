5dbf6b848f39512b1394d069152576e88aa27219
Vitor Shen <17490173+shenvitor@users.noreply.github.com>
2024-05-15 11:30:41 +0200

    sorting in config py file

--- docs/conf.py
+++ docs/conf.py
@@ -1,19 +1,11 @@
-# outline for a myst_nb project with sphinx
-# build with: sphinx-build -nW --keep-going -b html . ./_build/html
-
-# load extensions
-extensions = ["myst_nb"]
-
-# specify project details
-master_doc = "index"
-project = "GlueX N-Star"
-
-# basic build settings
 exclude_patterns = [
-    ".pixi",
-    "_build",
-    "Thumbs.db",
-    ".DS_Store",
     "**.ipynb_checkpoints",
+    ".DS_Store",
+    ".pixi",
+    "Thumbs.db",
+    "_build",
 ]
+extensions = ["myst_nb"]
+master_doc = "index"
 nitpicky = True
+project = "GlueX N-Star"
