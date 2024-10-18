0833282a3492bc03e30fec55173383fa4d2760fb
Remco de Boer <29308176+redeboer@users.noreply.github.com>
2024-10-18 15:03:25 +0200

    DOC: update Binder links

--- docs/conf.py
+++ docs/conf.py
@@ -40,7 +40,7 @@ html_theme_options = {
         },
         {
             "name": "Launch on Binder",
-            "url": f"https://mybinder.org/v2/gh/{ORGANIZATION}/{REPO_NAME}/main?filepath=docs",
+            "url": f"https://mybinder.org/v2/gh/{ORGANIZATION}/{REPO_NAME}/main?urlpath=lab",
             "icon": "https://mybinder.readthedocs.io/en/latest/_static/favicon.png",
             "type": "url",
         },
